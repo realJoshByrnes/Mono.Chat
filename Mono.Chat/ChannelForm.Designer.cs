ee4b77723a4bb769236d4826aad94c738eb7c089
realJoshByrnes <204185+realJoshByrnes@users.noreply.github.com>
2024-10-27 06:59:35 +1100

    Add statusbar (theme only)

--- Mono.Chat/ChannelForm.Designer.cs
+++ Mono.Chat/ChannelForm.Designer.cs
@@ -30,6 +30,9 @@
         {
             chatHeader1 = new ChatHeader();
             cfPanel = new Panel();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            statusStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // chatHeader1
@@ -45,8 +48,27 @@
             cfPanel.Dock = DockStyle.Fill;
             cfPanel.Location = new Point(0, 106);
             cfPanel.Name = "cfPanel";
-            cfPanel.Size = new Size(800, 422);
+            cfPanel.Size = new Size(800, 400);
             cfPanel.TabIndex = 1;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.BackColor = Color.FromArgb(214, 67, 0);
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
+            statusStrip1.Location = new Point(0, 506);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(800, 22);
+            statusStrip1.TabIndex = 2;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            toolStripStatusLabel1.Font = new Font("Tahoma", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            toolStripStatusLabel1.ForeColor = Color.White;
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(242, 17);
+            toolStripStatusLabel1.Text = "  Try MSN Internet Software for FREE!";
             // 
             // ChannelForm
             // 
@@ -55,14 +77,20 @@
             ClientSize = new Size(800, 528);
             Controls.Add(cfPanel);
             Controls.Add(chatHeader1);
+            Controls.Add(statusStrip1);
             Name = "ChannelForm";
             Text = "ChannelForm";
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private ChatHeader chatHeader1;
         public Panel cfPanel;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
     }
 }
